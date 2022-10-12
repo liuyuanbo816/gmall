b628dd4e8ac90cfd9d876ba459cc84db8ddd04bc
Zmz <Zmz@gmail.com>
2022-10-12 21:56:21 +0800

    main 添加测试工程

---
+++ others/src/main/java/gmall/others/OthersApp.java
@@ -0,0 +1,19 @@
+package gmall.others;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OthersApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OthersApp.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+}
